241bb9e845fea6dec9ea4318d2307f2398adb6ef
Jonathan Woltz <jwoltz13@gmail.com>
2018-04-07 11:06:36 -0400

    app.js has pseudocode

---
+++ assets/javascript/app.js
@@ -0,0 +1,15 @@
+// JS variables, such as "animalSearched", api-key, api-url;
+
+// Jquery on click event for submit button, should grab zip code box
+
+// Jquery event for clicking left arrow on the page 
+
+// Jquery event for clicking left arrow on their keyboard
+
+// Jquery event for right arrow on keyboard
+
+// Jquery event for clicking right arrow on the page
+
+// JS method for replacing entire page with new HTML
+
+// JS method for getting API request
