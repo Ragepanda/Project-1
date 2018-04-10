6486e36cdf098f8816b795642c4649417813fb8d
Neeni Nagaraj <neeninagaraj@Neenis-MacBook-Air.local>
2018-04-10 18:22:45 -0400

    update app.js

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,9 +1,15 @@
 // JS variables, such as "animalSearched", api-key, api-url;
 var apiKey = "49bd46fd5aab11a948d0cf49fdb22633";
 var animalSearched = "Dog";
+var apiUrl = "";
 // Jquery on click event for submit button, should grab zip code box
+$('#enter').on('click', function() {
+    var zipCode = $('#zipCode').val();
+    console.log(zipCode);    
+// Jquery event for clicking left arrow on the page
+$('').on('click', function(){
 
-// Jquery event for clicking left arrow on the page 
+})
 
 // Jquery event for clicking left arrow on their keyboard
 
@@ -12,5 +18,13 @@ var animalSearched = "Dog";
 // Jquery event for clicking right arrow on the page
 
 // JS method for replacing entire page with new HTML
-
+.innerHTML
 // JS method for getting API request
+var queryURL = "";
+
+$.ajax({
+    url: queryURL,
+    method: "GET"
+  }).then(function(response) {
+    console.log(response);
+  });
