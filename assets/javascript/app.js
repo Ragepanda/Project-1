ed3fff06a0cda39ba27d29c46886967a0759ee3d
Neeni Nagaraj <neeninagaraj@Neenis-MacBook-Air.local>
2018-04-10 18:51:16 -0400

    neeni's commit

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,11 +1,22 @@
 // JS variables, such as "animalSearched", api-key, api-url;
 var apiKey = "49bd46fd5aab11a948d0cf49fdb22633";
 var animalSearched = "Dog";
+var apiUrl = "";
 // Jquery on click event for submit button, should grab zip code box
+<<<<<<< HEAD
   $('#enter').on('click', function() {
         var zipCode = $('#zipCode').val();
         console.log(zipCode); 
 // Jquery event for clicking left arrow on the page 
+=======
+$('#enter').on('click', function() {
+    var zipCode = $('#zipCode').val();
+    console.log(zipCode);    
+// Jquery event for clicking left arrow on the page
+$('').on('click', function(){
+
+})
+>>>>>>> nagaraj
 
 // Jquery event for clicking left arrow on their keyboard
 
@@ -14,7 +25,11 @@ var animalSearched = "Dog";
 // Jquery event for clicking right arrow on the page
 
 // JS method for replacing entire page with new HTML
+.innerHTML
+// JS method for getting API request
+var queryURL = "";
 
+<<<<<<< HEAD
 // JS method for getting API request
 
 var queryURL = "http://api.petfinder.com/pet.getRandom?key="+apiKey+"&output=full&format=json&animal=dog";
@@ -29,3 +44,11 @@ $.ajax({
    
 
     });
+=======
+$.ajax({
+    url: queryURL,
+    method: "GET"
+  }).then(function(response) {
+    console.log(response);
+  });
+>>>>>>> nagaraj
