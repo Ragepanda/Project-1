479030ac72440cfeca443be486f2f4798f0ba720
Jonathan Woltz <jwoltz13@gmail.com>
2018-04-10 21:22:49 -0400

    added some testing code and extended API function

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,66 +2,73 @@
 var apiKey = "49bd46fd5aab11a948d0cf49fdb22633";
 var animalSearched = "Dog";
 var apiUrl = "";
+var returnValue;
 // Jquery on click event for submit button, should grab zip code box
 
-$('#submit').on('click', function() {
+$('#submit').on('click', function () {
   var name = $('#name').val();
+  var newVar;
   console.log(name);
 
   var zipCode = $('#zipCode').val();
-  console.log(zipCode);  
-  console.log(getDawgs());  
+  console.log(zipCode);
+  
+  getDawgs();
 });
-  var addClass = 'highlight-border';
-  var cols = $('.highlight').click(function () {
-      $(this).toggleClass(addClass);
-  });
 
-  function openNav() {
-      document.getElementById("mySidenav").style.width = "250px";
-      document.getElementById("main").style.marginLeft = "250px";
-  };
+var addClass = 'highlight-border';
+var cols = $('.highlight').click(function () {
+  $(this).toggleClass(addClass);
+});
 
-  function closeNav() {
-      document.getElementById("mySidenav").style.width = "0";
-      document.getElementById("main").style.marginLeft = "0";
-  };
- 
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+  document.getElementById("main").style.marginLeft = "250px";
+};
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+  document.getElementById("main").style.marginLeft = "0";
+};
+
 // Jquery event for clicking left arrow on their keyboard
-$('').on('click', function(){
+$('').on('click', function () {
 
 })
 
 // Jquery event for right arrow on keyboard
-$('').on('click', function(){
-    
+$('').on('click', function () {
+
 })
 // Jquery event for clicking right arrow on the page
-$('').on('click', function(){
-    
+$('').on('click', function () {
+
 })
 // JS method for replacing entire page with new HTML
 
 
 // JS method for getting API request
 
-function getDawgs(){
-var queryURL = "https://api.petfinder.com/pet.getRandom?key="+apiKey+"&output=full&format=json&animal=dog";
-var returnValue;
-  
+function getDawgs() {
+  var queryURL = "https://api.petfinder.com/pet.getRandom?key=" + apiKey + "&output=full&format=json&animal=dog";
+
   jQuery.ajax({
     type: 'GET',
     url: queryURL,
     dataType: 'jsonp'
-  }).then((response)=> {
-    console.log(response);
-    returnValue = response;
-  }).catch((error)=>{
+  }).then((response) => {
+    console.log(response.petfinder.pet.name.$t);
+    returnValue = response.petfinder;
+    console.log(returnValue);
+    return returnValue;
+  }).catch((error) => {
     console.log(error);
   });
 
-return returnValue;
- // console.log("end of getDawgs method");
+
+
+
+  // console.log("end of getDawgs method");
 }
 
 //getDawgs();
